f910220aba3c9ed987fc92834a3713b0d05b2258
phamduyvu <phamduyvu11@gmail.com>
2017-05-11 00:06:34 +0700

    Sử dụng ListView control với mảng dữ liệu định sẵn

---
+++ ListView_HardCode_Array/gen/com/PhamDuyVu/listview_hardcode_array/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.PhamDuyVu.listview_hardcode_array;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
